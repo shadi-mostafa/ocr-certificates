0eafc0f3df182c7fb3b6e14011651e545cd18db9
Shadi Mostafa <shadi.mostafa@alorubaschools.com>
2022-03-10 15:01:34 +0300

    initial commit

---
+++ image_to_data.py
@@ -0,0 +1,20 @@
+import re
+import cv2
+import pytesseract
+from pytesseract import Output
+
+img = cv2.imread('sample.jpg')
+d = pytesseract.image_to_data(img, output_type=Output.DICT)
+keys = list(d.keys())
+
+date_pattern = '(1|2)(\d{9})'
+
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 60:
+    	if re.match(date_pattern, d['text'][i]):
+            print(d['text'][i])
+#	        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+#	        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imwrite('img1.jpg', img)
